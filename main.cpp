8af4301ccab4911f8e62c38c85a80488ba78bd0d
Subigya Jyoti Panta <subigyapanta@gmail.com>
2017-04-29 13:39:33 +0700

    First commit with readme

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv/cv.hpp>
+#include "opencv2/features2d.hpp"
+#include <opencv2/xfeatures2d/nonfree.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+int main() {
+    cout << "Program started ..." << std::endl;
+    Mat img1 = imread("../me1.jpg", IMREAD_GRAYSCALE);
+    Mat img2 = imread("../me2.jpg", IMREAD_GRAYSCALE);
+
+    // can use SURT::create() or ORB::create() also
+    Ptr<Feature2D> f2d = SIFT::create();//SURF::create();//SIFT::create();
+
+    cout << "Getting Keypoints..." << endl;
+    // step 1: detect the keypoints
+    vector<KeyPoint> KeyPoints1, KeyPoints2;
+    f2d->detect(img1, KeyPoints1);
+    f2d->detect(img2, KeyPoints2);
+
+    cout << "Getting features..." << endl;
+    // step 2: calculate descriptors (feature vectors)
+    Mat features1, features2;
+    f2d->compute(img1, KeyPoints1, features1);
+    f2d->compute(img2, KeyPoints2, features2);
+
+    // just print some statistics returned
+    Size size1 = features1.size();
+    Size size2 = features2.size();
+    cout << "Size of features1 is; " << size1 << endl;
+    cout << "Size of features2 is; " << size2 << endl;
+
+    // step 3: Matching features(descriptors) using BFMatcher
+    BFMatcher matcher;
+    vector<DMatch> matches;
+    Mat output;
+    matcher.match(features1, features2, matches);
+
+    // step 4: showing the matches in window
+    drawMatches(img1, KeyPoints1, img2, KeyPoints2, matches, output);
+    namedWindow("output", CV_WINDOW_NORMAL);
+    imshow("output", output);
+
+    waitKey(0);
+}
