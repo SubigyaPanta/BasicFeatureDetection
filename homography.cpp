031eface16bcee3b8be5beb83acede336cabab22
Subigya Jyoti Panta <subigyapanta@gmail.com>
2017-04-30 01:06:10 +0700

    Added homography.cpp

---
+++ homography.cpp
@@ -0,0 +1,23 @@
+//
+// Created by subigya on 4/30/17.
+//
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv/cv.hpp>
+#include "opencv2/features2d.hpp"
+#include <opencv2/xfeatures2d/nonfree.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+int main(){
+    Point2f pt = Point2f(4.4122e+02, 3.5669e+02);
+    cout<< pt<< endl;
+    return 0;
+}
+
+Mat getData(){
+//    Mat data = [];
+}
